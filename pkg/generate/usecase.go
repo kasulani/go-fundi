023321c1cb02428f215af416d0c4c79bbcefb7f3
Emmanuel King Kasulani <kasulani@gmail.com>
2021-09-03 23:02:30 +0200

    add use case to generate empty files

--- pkg/generate/usecase.go
+++ pkg/generate/usecase.go
@@ -16,12 +16,19 @@ type (
 		fundiFile FundiFileReader
 		hCreator  HierarchyCreator
 	}
+
+	// EmptyFiles use case type.
+	EmptyFiles struct {
+		fileReader FundiFileReader
+		fCreator   FileCreator
+	}
 )
 
 // ProvideUseCases returns a DI container option with use case types.
 func ProvideUseCases() di.Option {
 	return di.Options(
 		di.Provide(NewDirectoryStructure),
+		di.Provide(NewEmptyFiles),
 	)
 }
 
@@ -35,20 +42,49 @@ func NewDirectoryStructure(
 	}
 }
 
+// NewEmptyFiles returns an instance of EmptyFiles.
+func NewEmptyFiles(reader FundiFileReader, creator FileCreator) *EmptyFiles {
+	return &EmptyFiles{
+		fileReader: reader,
+		fCreator:   creator,
+	}
+}
+
 // UseCase to generate an empty directory structure.
 func (ps *DirectoryStructure) UseCase() error {
-	project, err := ps.fundiFile.Read()
+	fundiFile, err := ps.fundiFile.Read()
 	if err != nil {
 		return errors.Wrap(err, "failed to read fundi file")
 	}
 
-	directories, err := getAllDirectories(project.Structure)
+	directories, err := getAllDirectories(fundiFile.Structure)
 	if err != nil {
 		return errors.Wrap(err, "failed to get directories")
 	}
 
-	if err := ps.hCreator.CreateHierarchy(generateHierarchy(project.Metadata.Path, directories)); err != nil {
+	if err := ps.hCreator.CreateHierarchy(generateHierarchy(fundiFile.Metadata.Path, directories)); err != nil {
 		return errors.Wrap(err, "failed to create directory hierarchy")
+	}
+
+	return nil
+}
+
+// UseCase to add empty files to an existing directory structure.
+func (ef *EmptyFiles) UseCase() error {
+	fundiFile, err := ef.fileReader.Read()
+	if err != nil {
+		return errors.Wrap(err, "failed to read fundi file")
+	}
+
+	files, err := getFilesSkipTemplates(fundiFile.Structure)
+	if err != nil {
+		return errors.Wrap(err, "failed to get directories")
+	}
+
+	if err := ef.fCreator.CreateFiles(
+		generateEmptyFiles(generateHierarchy(fundiFile.Metadata.Path, files)),
+	); err != nil {
+		return errors.Wrap(err, "failed to add empty files to directory structure")
 	}
 
 	return nil
